f5c045f0bab6ff490db6cb1ef996913115dbc62a
tanovese <82059748+tanovese@users.noreply.github.com>
2023-05-29 13:46:20 -0400

    Added css, started moonphase js

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+var lat = 40.735657;
+var lon = -74.172364;
+const applicationId = "2783890d-6a79-4a53-85ea-a093142ad152";
+const applicationSecret ="31acc37032ad69c4d5f7928586e995f9f30116465cf5dbc9669b235b5d71362584d5ba854089cc09e823e2052b7d0b4d2a30ed06d6e1ca2bf2995fcfae759c8f8004d66ad88d304c3219be628bf106d4f2a6ccd2e52fa416d1d575ddeb9e87d9536f373b6af2e372b0f92e7a1f6478ed";
+const authString = btoa(`${applicationId}:${applicationSecret}`);
+
+document.addEventListener("DOMContentLoaded", function () {
+  var client = new AstronomyAPI({
+    basicToken: `${authString}`,
+  });
+
+  client.moonPhase();
+});
+
+async function fetchData() {
+
+
+
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${authString}`,
+    },
+    body: JSON.stringify({
+      "style": getStyle(),
+      "observer": {
+          "latitude": lat,
+          "longitude": lon,
+          "date": getDateString()
+      },
+      "view": {
+          "type": "area",
+          "parameters": {
+              "position": {
+                  "equatorial": {
+                      "rightAscension": 0,
+                      "declination": 0
+                  }
+              },
+              "zoom": 3 //optional
+          }
+      }
+      })
+  };
+
+  const url = "https://api.astronomyapi.com/api/v2/studio/star-chart";
+  await fetch(url, options)
+    .then((response) => response.json())
+    .then((responseData) => displayStarChart(responseData.data));
+}
+
+function displayStarChart(data) {
+    console.log(data);
+    console.log(data.imageUrl);
+    document.getElementById("star-chart").src = data.imageUrl;
+}
+
+function getStyle() {
+  return document.getElementById("style").value;
+}
+
+function getDateString() {
+  return dayjs().format("YYYY-MM-DD");
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,39 @@
-# Project-1
+# My Stars Team Project
+
+## Installation
+
+## Usage
+This web application .....  
+User Story for information to build usage section  
+
+As a star-gazer, I want to be able to pick a city and state to travel to  
+So that I can see what stars or constellations are visible from that location
+
+As a star-gazer, I want to be able to save that location  
+So that I can go back to it to help me decide where to go
+
+As a star-gazer, I want to be able to view the cloud cover status (see what the ratings are) and precipitation  
+So that I can decide if I will be able to see the stars or constellations from my location
+
+As a star-gazer, I want to see a description of the hourly weather  
+So that I can decide what hours in the evening will be best for viewing the stars or constellations
+
+As a star-gazer, I want to be able to choose to view the entire star sky or a single constellation  
+So that I can better decide if this location is a good viewing location
+
+As a star-gazer, I want the ability to view the sky or a constellation at each hour  
+So that I can see where in the constellation or stars are in the night sky when I am viewing
+
+As a star-gazer, I want to be able to pick a constellation from a list and view that constellation  
+So that I can determine if and where I can see that constellation.
+
+As a user, I want to be able to view the night sky, and a specific constellation  
+So that I can get my bearings of the constellation
+
+
+
+## Visual Description
+
+## Collaborators
+
+## License
