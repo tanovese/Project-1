1c3f2e2a16aaed1bf1ffbbb0987cb40f1d227bbe
hannahrajarao <hannahrajarao@gmail.com>
2023-05-30 15:18:07 -0400

    Insert code to display map

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,6 +66,40 @@ async function locationToCoordinates(locationString) {
     })
     .catch(error => console.log(error));
   }
+
+function displayMap(lat, lon) {
+  console.log(lat, lon);
+  // Create a Leaflet map centered on the location
+  const map = L.map('map').setView([lat, lon], 12);
+  // Add a tile layer to the map 
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+  maxZoom: 18,
+  }).addTo(map);
+  // Add a tile layer for the cloud layer 
+  var cloudLayer = L.tileLayer(`https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=${apiKey}`,
+  {
+    attribution: '',
+    filter: [
+      'brightness:80%',
+      'contrast:100%',
+      'saturate:100%',
+      'hue:270deg',
+      'sepia:100%',
+      'opacity:1.0'
+    ],
+    palette: {
+    }
+  }
+  ).addTo(map);
+  // Add tile layer for precipitation   
+  var precipitationLayer = L.tileLayer(`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=${apiKey}`, {
+  opacity: 1,
+  attribution: '<a href="https://openweathermap.org/">OpenWeatherMap</a>',
+  }).addTo(map);
+  // Add a marker to the map at the location
+  L.marker([lat, lon]).addTo(map);
+  fetchStarChart();
 }
 
 async function fetchStarChart() {
