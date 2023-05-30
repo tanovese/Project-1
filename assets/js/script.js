ccda7a9a5b5c1b79d53e77ee76c0863947c17d20
Moebirdie <93432701+Moebirdie@users.noreply.github.com>
2023-05-30 08:57:48 -0400

    removed duplicate variables. Careful merging. Files were merged into this branch.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,81 @@
-var lat = 40.735657;
-var lon = -74.172364;
+var stateString = ""
+var valueToFetchWeather = ""
 
-async function fetchData() {
-  const applicationId = "2783890d-6a79-4a53-85ea-a093142ad152";
-  const applicationSecret ="31acc37032ad69c4d5f7928586e995f9f30116465cf5dbc9669b235b5d71362584d5ba854089cc09e823e2052b7d0b4d2a30ed06d6e1ca2bf2995fcfae759c8f8004d66ad88d304c3219be628bf106d4f2a6ccd2e52fa416d1d575ddeb9e87d9536f373b6af2e372b0f92e7a1f6478ed";
-  const authString = btoa(`${applicationId}:${applicationSecret}`);
-
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Basic ${authString}`,
-    },
-    body: JSON.stringify({
-      "style": getStyle(),
-      "observer": {
-          "latitude": lat,
-          "longitude": lon,
-          "date": getDateString()
-      },
-      "view": {
-          "type": "area",
-          "parameters": {
-              "position": {
-                  "equatorial": {
-                      "rightAscension": 0,
-                      "declination": 0
-                  }
-              },
-              "zoom": 3 //optional
-          }
-      }
-      })
-  };
-
-  const url = "https://api.astronomyapi.com/api/v2/studio/star-chart";
-  await fetch(url, options)
-    .then((response) => response.json())
-    .then((responseData) => displayStarChart(responseData.data));
+function getStateString(state) {
+    if (state === "") {
+        stateString = "";
+        return stateString;
+    }
+    else {
+        stateString = `${state},`;
+        return stateString;
+    }
+    console.log(stateString);
 }
 
-function displayStarChart(data) {
-    console.log(data);
-    console.log(data.imageUrl);
-    document.getElementById("star-chart").src = data.imageUrl;
+function searchLocation() {
+    var cityInput = document.getElementById("locationCity");
+    var cityVal = cityInput.value;
+    var cityEncodedVal = encodeURIComponent(cityVal);
+    var cityToFetch = `${cityEncodedVal},`;
+    var stateInput = document.getElementById("locationState");
+    var stateValue = stateInput.value;
+    var countryInput = document.getElementById("countrySelect");
+    var countryValue = countryInput.value;
+
+    getStateString(stateValue);
+
+    valueToFetchWeather = `${cityToFetch}${stateString}${countryValue}`
+    console.log(cityEncodedVal, countryValue, valueToFetchWeather);
+
+    getWeatherAndMaps(valueToFetchWeather);
 }
 
-function getStyle() {
-  return document.getElementById("style").value;
+const apiKey = 'c65a83f1b41423a44ca059c4924fe1cd';
+
+//Call the weather 
+
+function getWeatherAndMaps() {
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${valueToFetchWeather}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+        // Extract the coordinates of the location
+        var { lat, lon } = data.coord;
+        console.log(data);
+        console.log(lat, lon);
+        // Create a base map centered on the location - leaflet.js
+        const map = L.map('map').setView([lat, lon], 12);
+        // Add a tile layer to the map 
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {  
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+            maxZoom: 18,
+        }).addTo(map);
+        // Add a tile layer for the cloud layer  - from openweatherapi
+        var cloudLayer = L.tileLayer(`https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=${apiKey}`,
+            {
+                attribution: '',
+                filter: [
+                    'brightness:80%',
+                    'contrast:100%',
+                    'saturate:100%',
+                    'hue:270deg',
+                    'sepia:100%',
+                    'opacity:1.0'
+                ],
+                palette: {
+                }
+            }
+        ).addTo(map);
+        // Add tile layer for precipitation   - from openweatherapi 
+        var precipitationLayer = L.tileLayer(`https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=${apiKey}`, {
+            opacity: 1,
+            attribution: '<a href="https://openweathermap.org/">OpenWeatherMap</a>',
+        }).addTo(map);
+        // Add a marker to the map at the location
+        L.marker([lat, lon]).addTo(map);
+    })
+    .catch(error => console.log(error));
 }
 
-function getDateString() {
-  return dayjs().format("YYYY-MM-DD");
-}
+// Search button
+document.getElementById("searchLocation").addEventListener("click", searchLocation);
