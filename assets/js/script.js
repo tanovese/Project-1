aab30365e7ad81c23fe8f2cf91fd4debd9ce2a67
hannahrajarao <hannahrajarao@gmail.com>
2023-06-01 21:37:41 -0400

    Rename url and options

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,8 +135,9 @@ async function fetchStarChart() {
   const applicationId = "2783890d-6a79-4a53-85ea-a093142ad152";
   const applicationSecret ="31acc37032ad69c4d5f7928586e995f9f30116465cf5dbc9669b235b5d71362584d5ba854089cc09e823e2052b7d0b4d2a30ed06d6e1ca2bf2995fcfae759c8f8004d66ad88d304c3219be628bf106d4f2a6ccd2e52fa416d1d575ddeb9e87d9536f373b6af2e372b0f92e7a1f6478ed";
   const authString = btoa(`${applicationId}:${applicationSecret}`);
+  const url = "https://api.astronomyapi.com/api/v2/studio/";
 
-  const options = {
+  const starOptions = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -163,11 +164,11 @@ async function fetchStarChart() {
       }
       })
   };
-  console.log(options.body);
-  const url = "https://api.astronomyapi.com/api/v2/studio/star-chart";
+  console.log(starOptions.body);
+  const starUrl = url + 'star-chart';
   startTime = new Date();
   console.log('timer started');
-  await fetch(url, options)
+  await fetch(starUrl, starOptions)
     .then((response) => response.json())
     .then((responseData) => displayStarChart(responseData.data));
   submitButton.disabled = false;
