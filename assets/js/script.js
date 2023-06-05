8ec541482c2aaa9751c30c7936aef1888288c3e3
hannahrajarao <hannahrajarao@gmail.com>
2023-06-04 22:44:17 -0400

    Hard code star chart zoom and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,7 +145,7 @@ async function fetchStarChartAndMoonPhase() {
       Authorization: `Basic ${authString}`,
     },
     body: JSON.stringify({
-      "style": inputs.style,
+      "style": "default",
       "observer": {
         "latitude": inputs.latitude,
         "longitude": inputs.longitude,
@@ -160,7 +160,7 @@ async function fetchStarChartAndMoonPhase() {
               "declination": 0
             }
           },
-          "zoom": inputs.zoom
+          "zoom": 4
         }
       }
     })
