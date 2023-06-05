6f1c67a330d75d383cc28b81d78c568ebdddf694
hannahrajarao <hannahrajarao@gmail.com>
2023-06-05 00:10:15 -0400

    Separate and rename astronomy fetch functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,15 +132,11 @@ function displayMap(lat, lon) {
   }).addTo(map);
   // Add a marker to the map at the location
   L.marker([lat, lon]).addTo(map);
-  fetchStarChartAndMoonPhase();
+  fetchStarChart();
 }
 
-async function fetchStarChartAndMoonPhase() {
+async function fetchStarChart() {
   submitButton.disabled = true;
-  const applicationId = "2783890d-6a79-4a53-85ea-a093142ad152";
-  const applicationSecret = "31acc37032ad69c4d5f7928586e995f9f30116465cf5dbc9669b235b5d71362584d5ba854089cc09e823e2052b7d0b4d2a30ed06d6e1ca2bf2995fcfae759c8f8004d66ad88d304c3219be628bf106d4f2a6ccd2e52fa416d1d575ddeb9e87d9536f373b6af2e372b0f92e7a1f6478ed";
-  const authString = btoa(`${applicationId}:${applicationSecret}`);
-  const url = "https://api.astronomyapi.com/api/v2/studio/";
 
   const starOptions = {
     method: 'POST',
@@ -180,6 +176,7 @@ async function fetchStarChartAndMoonPhase() {
     .catch(error => console.log(error));;
 }
 
+async function fetchMoonPhase() {
   const moonOptions = {
     method: 'POST',
     headers: {
